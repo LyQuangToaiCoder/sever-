a6b7ee6d4cd9faae9b79aabc1381a52c3a49bbc9
LyQuangToaiCoder <106007459+LyQuangToaiCoder@users.noreply.github.com>
2022-05-24 08:25:59 +0700

    Add files via upload

---
+++ keytool.php
@@ -0,0 +1,46 @@
+<html>
+<body>
+<br>
+<br>
+<br>
+<br>
+<b>
+<span style="font-family: Impact">
+<h1><center><span style="font-size:120px">THANK YOU</span></center></h1>
+</span>
+</b>
+<br>
+<center>
+<img src="https://gifimage.net/wp-content/uploads/2017/11/gambar-gif-doraemon-8.gif" style=“width:250px;height:250px;”>
+</center>
+<br>
+<br>
+<span style="font-family: Comic Sans MS">
+<span style="font-size:24px">
+<?php
+ 
+        $key = $_GET['key'];
+        
+        
+        
+       
+
+ echo "<center>";
+ echo "Xin Chào! Cảm Ơn Bạn Đã Vượt Link Rút Gọn. Hãy Cùng Nhau Phát Triển Cộng Đồng Nhé"; echo "<br>";
+ echo "API Key Ngày ";
+ echo date('d/m/Y');
+ echo " Là: "; echo "<br><b>";
+ echo $key; echo "</b>";
+ echo "<br>";
+ echo "<br>";
+ echo "<br>";
+echo "</center>";
+?>
+</span>
+</span>
+<span style="font-family: Comic Sans MS">
+<h><center><span style="font-size:15px">Copyright ©2021 | YOUTUBE :<a href="https://www.youtube.com/channel/UCEg--59HYZyZlspVLo6H7Lg"> Lê Văn Bình</a></span></center></h>
+</span>
+</body>
+
+</html>
